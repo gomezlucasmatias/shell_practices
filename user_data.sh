3c6873bbc6b028a25c0bf025dcbffb3205bcce42
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-26 22:32:11 -0300

    Add space after question

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Hola forastero! Como te llamas?" nombre
-read -p "Cuantos años dices tener?" edad
+read -p "Hola forastero! Como te llamas? " nombre
+read -p "Cuantos años dices tener? " edad
 
 echo "Entonces puedo decirte querido $nombre que dentro de un año vas a tener $((edad + 1))"
