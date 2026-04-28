f4749a28a53000a028fb523359b267f7d518df3f
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-28 20:57:09 -0300

    No work

---
+++ imc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Solicita datos al paciente
+read -p "Dame tu peso en kg " peso
+read -p "Dame tu altura en Mts " altura
+
+# Controlar que el peso y la altura no sean negativos o 0
+if [ $peso -gt 0 && $altura -gt 0 ]; then
+  imc=$(echo "scale=1; $peso / ($altura * $altura)" | bc)
+fi
+
+# Mostrar imc
+echo $imc
+
+if [ $imc -lt 18.5 ]; then
+  echo "Con $imc estás bajo de peso 💀"
+elif [ $imc -ge 18.5 && $imc -le 24.9 ]; then
+  echo "Con $imc tu peso es normal ☺️"
+elif [ $imc -ge 25.0 && $imc -le 29.9 ]; then
+  echo "Con $imc tienes sobrepeso 😵"
+else
+  echo "Con $imc tienes obesidad 🫨"
+fi
