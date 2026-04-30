187932c7b73037e02b7989fb135a90ee15304203
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-29 21:44:11 -0300

    Add IMC calculator with validation and bc

--- imc.sh
+++ imc.sh
@@ -4,7 +4,7 @@ read -p "Dame tu peso en kg " peso
 read -p "Dame tu altura en Mts " altura
 
 # Controlar que el peso y la altura no sean negativos o 0
-validador=$(echo "peso > 0 && altura > 0" | bc -l)
+validador=$(echo "$peso > 0 && $altura > 0" | bc -l)
 if [ $validador -eq 1 ]; then
   imc=$(echo "scale=1; $peso / ($altura * $altura)" | bc)
 fi
@@ -12,11 +12,11 @@ fi
 # Mostrar imc
 echo "$imc"
 
-if [ $imc -lt 18.5 ]; then
+if (( $(echo "$imc < 18.5" | bc -l) )); then
   echo "Con $imc estás bajo de peso 💀"
-elif [ $imc -ge 18.5 && $imc -le 24.9 ]; then
-  echo "Con $imc tu peso es normal ☺️"
-elif [ $imc -ge 25.0 && $imc -le 29.9 ]; then
+elif (( $(echo "$imc >= 18.5 && $imc <= 24.9" | bc -l) )); then
+  echo "Con $imc eres normal ☺️"
+elif (( $(echo "$imc >= 25.0 && $imc <= 29.9" | bc -l) )); then
   echo "Con $imc tienes sobrepeso 😵"
 else
   echo "Con $imc tienes obesidad 🫨"
