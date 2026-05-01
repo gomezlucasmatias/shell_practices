696b42ed1d65d938c587ae27608e26bff73c0bc1
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-30 23:48:31 -0300

    Add verificator

--- operations.sh
+++ operations.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 read -p "Dame un numero " num
 
+if [[ ! "$num" =~ ^-?[0-9]+$ ]]; then
+  echo "Error: No ingresaste un número entero válido"
+  exit 1
+fi
+
 if [ $num -gt 0 ]; then
   echo "Es positivo y $(($num * 2)) es su doble"
 elif [ $num -lt 0 ]; then
